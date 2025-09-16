5520f53aee1ff21f3b0d9f482c099c2cfc36d69c
Arsen.Danilov <arsen.danilov@ite.group>
2025-09-16 11:18:31 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <div className="text-3xl font-bold underline">
+        Test Test Test
+        <Button>Life</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
