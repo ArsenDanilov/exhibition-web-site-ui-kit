50f9d2d31ac3e6414300eb1d52a49b8374865eb4
Arsen.Danilov <arsen.danilov@ite.group>
2025-09-23 18:31:57 +0300

    fix: props names at Text.tsx

--- src/components/ui/Text.tsx
+++ src/components/ui/Text.tsx
@@ -1,15 +1,15 @@
 import { memo } from "react";
 
-interface TextProps extends HTMLElement {
-    text: string,
+interface TextProps {
+    children: React.ReactNode;
     tag?: 'p' | 'span' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6',
-    size?: 'xs' | 'sm' | 'base' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl';
+    textStyle?: 'xs' | 'sm' | 'base' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl';
 }
 
-export const Text = memo(({ text, tag = 'p', size = 'base' }: TextProps) => {
+export const Text = memo(({ children, tag = 'p', textStyle = 'base' }: TextProps) => {
     const Component = tag;
 
     return (
-        <Component className={`${size}`}>{text}</Component>
+        <Component className={`${textStyle}`}>{children}</Component>
     )
 })
