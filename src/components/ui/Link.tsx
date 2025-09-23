e55805112d04de337291611208ed9d38810a4e9c
Arsen.Danilov <arsen.danilov@ite.group>
2025-09-23 18:50:10 +0300

    feature: add Link component

---
+++ src/components/ui/Link.tsx
@@ -0,0 +1,13 @@
+import { memo } from "react"
+
+interface LinkProps {
+    children: React.ReactNode,
+    href: string, 
+    className?: string
+}
+
+export const Link = memo(({ children, href, className, ...props }: LinkProps) => {
+    return (
+        <a href={href} className={className} {...props}>{children}</a>
+    )
+})
