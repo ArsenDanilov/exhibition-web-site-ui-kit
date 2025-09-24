27d2cf094b3a19de34f590de46335ba0398ca747
Arsen.Danilov <arsen.danilov@ite.group>
2025-09-24 18:44:15 +0300

    fix: add correct breakpoints

--- src/components/layout/navbar/DesktopNavbar.tsx
+++ src/components/layout/navbar/DesktopNavbar.tsx
@@ -5,17 +5,23 @@ import type { INavbarData } from "./navbar.types";
 
 export const DesktopNavbar = ({ data }: { data: INavbarData[] }) => {
   return (
-    <nav className="hidden desktop:flex items-center justify-between px-7 py-5 rounded-3xl">
-        <Logo />
-        <div className="flex items-center gap-6">
-          {data.map((item) => (
-            <Link key={item.href} href={item.href} className="text-blackblur md:hover:text-black">{item.title}</Link>
-          ))}
-        </div>
-        <div className="flex items-center justify-center gap-2">
-          <Button>Sign In</Button>
-          <Button variant="secondary">Register</Button>
-        </div>
-      </nav>
+    <nav className="hidden items-center justify-between py-[18px] px-[25px] rounded-3xl md:flex md:px-[80px] mdl:px-[108px] lg:px-[24px] ">
+      <Logo />
+      <div className="flex items-center gap-6">
+        {data.map((item) => (
+          <Link
+            key={item.href}
+            href={item.href}
+            className="text-blackblur md:hover:text-black"
+          >
+            {item.title}
+          </Link>
+        ))}
+      </div>
+      <div className="hidden lg:flex items-center justify-center gap-2">
+        <Button>Sign In</Button>
+        <Button variant="secondary">Register</Button>
+      </div>
+    </nav>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -74,21 +74,12 @@ export default {
         },
       },
       screens: {
-        desktop: "880px",
-      },
-      keyframes: {
-        'slide-out-to-top': {
-          from: { transform: 'translateY(0)' },
-          to: { transform: 'translateY(-100%)' },
-        },
-        'slide-in-from-top': {
-          from: { transform: 'translateY(-100%)' },
-          to: { transform: 'translateY(0)' },
-        },
-      },
-      animation: {
-        'slide-out-to-top': 'slide-out-to-top 0.3s ease-in-out forwards',
-        'slide-in-from-top': 'slide-in-from-top 0.5s ease-in-out forwards',
+        sm: "576px",
+        md: "768px",
+        mdl: "834px",
+        lg: "992px",
+        xl: "1200px",
+        xxl: "1400px",
       },
     },
   },
