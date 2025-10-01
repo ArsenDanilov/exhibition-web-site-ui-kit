f6f28740f9677b2a6a9ac75950347e77b3da9737
Arsen.Danilov <arsen.danilov@ite.group>
2025-10-01 15:39:12 +0300

    fix: remove width at Sessions.tsx

--- src/components/cards/sessions/Sessions.tsx
+++ src/components/cards/sessions/Sessions.tsx
@@ -4,7 +4,7 @@ import { Text } from "@/components/ui/Text";
 
 export const Sessions = () => {
     return (
-        <div className="px-7 py-5 w-[381px]">
+        <div className="px-7 py-5">
             <Text tag="h3" textStyle="3xl" className="mb-5 font-semibold">Sessions</Text>
             {sessionsData.map((data, index) => (
                 <SessionCard key={index} title={data.title} description={data.description} postedAt={data.postedAt} tags={data.tags} />
