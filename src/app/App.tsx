5c334d891740123de282e97025e0e8987bbbff78
Arsen.Danilov <arsen.danilov@ite.group>
2025-10-03 13:12:28 +0300

    fix: update Footer component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,8 @@
 import { About } from "@/components/layout/about/About";
 import { MainSection } from "@/components/layout/mainSection/MainSection";
 import { Navbar } from "@/components/layout/navbar/Navbar";
-import { Join } from "../components/layout/join/Join";
+import { Join } from "@/components/layout/join/Join";
+import { Footer } from "@/components/layout/footer/Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <About {...About.schema.getDefaultProps()} />
         <MainSection />
         <Join {...Join.schema.getDefaultProps()} />
+        <Footer {...Footer.schema.getDefaultProps()} />
       </div>
     </>
   );
--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,19 +1,41 @@
-import { Link } from "@/components//ui/Link"
-import { Logo } from "@/components//ui/Logo"
-import { Text } from "@/components//ui/Text"
-import { footerData } from "./footer.data"
+import { Link } from "@/components/ui/Link";
+import { Logo } from "@/components/ui/Logo";
+import { Text } from "@/components/ui/Text";
+import { type EditableComponent } from "../../../types";
 
-export const Footer = () => {
-    return (
-        <footer className="px-11 pt-7 pb-6 flex justify-between items-center border-black30">
-            <Logo />
-            <div className="flex justify-center gap-5">
-                {footerData.map((item) => (
-                    <Link href={item.href} key={item.href}>
-                        <Text textStyle="sm" className="text-black30">{item.title}</Text>
-                    </Link>
-                ))}
-            </div>
-        </footer>
-    )
+interface FooterLink {
+  title: string;
+  href: string;
 }
+
+interface FooterProps {
+  links: FooterLink[];
+}
+
+export const Footer: EditableComponent<FooterProps> = ({ links }) => {
+  return (
+    <footer className="px-11 pt-7 pb-6 flex justify-between items-center border-black30">
+      <Logo />
+      <div className="flex justify-center gap-5">
+        {links.map((link) => (
+          <Link href={link.href} key={link.href}>
+            <Text textStyle="sm" className="text-black30">
+              {link.title}
+            </Text>
+          </Link>
+        ))}
+      </div>
+    </footer>
+  );
+};
+
+Footer.schema = {
+  label: "Footer",
+  type: "Footer",
+  getDefaultProps: () => ({
+    links: [
+      { title: "Правила пользования платформой", href: "/rules" },
+      { title: "Политика использования данных", href: "/politics" },
+    ],
+  }),
+};
