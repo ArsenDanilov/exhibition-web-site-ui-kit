54665176577932f3f5a89843c6f14da07c05d1f8
Arsen.Danilov <arsen.danilov@ite.group>
2025-10-07 10:57:45 +0300

    fix: remove files data and types of footer

--- src/components/layout/footer/footer.data.ts
+++
@@ -1,6 +0,0 @@
-import { type IFooterData } from "./footer.types"
-
-export const footerData: IFooterData[] = [
-    { title: "Правила пользования платформой", href: "/rules" },
-    { title: "Политика использования данных", href: "/politics" },
-]
--- src/components/layout/footer/footer.types.ts
+++
@@ -1,4 +0,0 @@
-export interface IFooterData {
-    title: string,
-    href: string,
-}
